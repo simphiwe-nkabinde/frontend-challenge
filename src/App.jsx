69fd964dd8d1c8e6c6649be99c863f833a1ebab9
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2023-10-12 13:50:26 +0200

    home page: setup page boilerplate, create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import "bootstrap/dist/js/bootstrap.min.js";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className='bg-primary px-3' style={{height:'80px'}}>
+        <nav className='d-flex justify-content-between align-items-center py-2 px-3 h-100'>
+          <a href="">
+            <img src="/logo.svg" alt="" />
+          </a>
+          <div className='small fw-bold'>
+            <a className='text-light text-decoration-none py-2 px-3' href="">
+              Service
+              </a>
+            <a className='text-light text-decoration-none py-2 px-3' href="">
+              Industries</a>
+            <a className='text-light text-decoration-none py-2 px-3' href="">
+              Cases
+            </a>
+            <a className='text-light text-decoration-none py-2 px-3' href="">
+              Contact
+            </a>
+          </div>
+          <button className='btn btn-sm py-2 px-3 text-primary fw-bold bg-white rounded-pill'>
+            Let's Talk
+          </button>
+        </nav>
+      </header>
+      <main>
+        <section>
+          
+        </section>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+      <footer>
+
+      </footer>
     </>
   )
 }
