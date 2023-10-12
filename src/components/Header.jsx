9eb55b2fbdadf19d15aacb3ae936cc72f8eba1aa
Simphiwe Nkabinde <simphiwe.kabini@gmail.com>
2023-10-12 14:54:58 +0200

    responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,20 +9,21 @@ const navItems = [
 
 export default function Header() {
     return (
-        <header className='bg-primary px-3 d-flex align-items-center' style={{ minHeight: '80px' }}>
+        <header className='bg-primar px-3 d-flex align-items-center' style={{minHeight: '80px'}}>
             <nav className='navbar navbar-expand-md align-items-center py-2 px-3 h-100 w-100'>
                 <a className="navbar-brand" href="#">
-                    <img src="/logo.svg" alt="" />
+                    <img className='d-none d-md-inline' src="/logo.svg" alt="" />
+                    <img className='d-md-none' src="/logo-dark.svg" alt="" />
                 </a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigationBar" aria-controls="navigationBar" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navigationBar" aria-controls="navigationBar" aria-expanded="false" aria-label="Toggle navigation">
+                    <img src="hamburger.svg" alt="" />
                 </button>
                 <div className='mt-0 mt-md-0 small fw-bold collapse navbar-collapse justify-content-around' id='navigationBar'>
                     <ul className='navbar-nav text-light'>
                         {
                             navItems.map((navLink, index) => (
                                 <li className='nav-item py-2 px-3'>
-                                    <a className='nav-item text-light text-decoration-none text-capitalize'
+                                    <a className='nav-item text-primary text-decoration-none text-capitalize'
                                         aria-current="page"
                                         href={navLink.path}>
                                         {navLink.name}
@@ -31,7 +32,7 @@ export default function Header() {
                             ))
                         }
                     </ul>
-                    <button className='d-md-none my-2 btn btn-sm py-2 px-3 text-primary fw-bold bg-white rounded-pill'>
+                    <button className='d-md-none my-2 btn btn-sm py-2 px-3 text-light fw-bold bg-primary rounded-pill'>
                         Let's Talk
                     </button>
                 </div>
